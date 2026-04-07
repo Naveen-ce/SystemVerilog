72d2cf3053ac83978fa08d218842858ad540ac9f
Naveen <naveenmurugesan1972@gmail.com>
2026-04-07 09:52:07 +0530

    Add files via upload

---
+++ 01_Data_types/Typedef/Enumeration1_typedef.sv
@@ -0,0 +1,40 @@
+module tb;
+  typedef enum {Red,Green,Yellow} color;
+  
+  typedef enum {Green[5]} color1;
+  
+  typedef enum {Pink[3]=6} color3;
+  
+  initial begin
+    
+    color state1;
+    color1 state2;
+    color3 state3;
+    
+    state1=Red;
+    $display("Color=%0d | Color_name=%s",state1,state1.name());
+    
+    
+    state2=Green2;         
+    $display("Color=%0d | Color_name=%s",state2,state2.name());
+    
+    state3=Pink2;
+    $display("Color=%0d | Color_name=%s",state3,state3.name());
+  end
+endmodule
+    
+
+/* OUTPUT
+
+Color=0 | Color_name=Red
+Color=2 | Color_name=Green2
+Color=8 | Color_name=Pink2
+xmsim: *W,RNQUIE: Simulation is complete.
+*/
+  
+  
+                
+                
+                
+               
+
